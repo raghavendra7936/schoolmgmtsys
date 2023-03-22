c57f57a3393e39493dbec43a6e68d5d6e16de946
raghavendra7936 <raghavendra.rao1408@gmail.com>
2023-03-22 10:18:13 +0530

    updated settings

--- schoolmgmtsys/settings.py
+++ schoolmgmtsys/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--g+t+yq_%*-l9xt+q9^+6*-z@65-$58m+*x5r3k5wckg@7++uo'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
